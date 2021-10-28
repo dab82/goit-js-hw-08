2c683c085c09d2ed4d63c66210662f665bd1d9f4
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-10-28 23:20:23 +0300

    task 01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
 
@@ -8,59 +8,17 @@ const galleryContainer = document.querySelector('.gallery');
 
 const imgMarkup = galleryItems
   .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-	            <a class="gallery__link" href="${original}">
-		            <img
-			            class="gallery__image"
-			            src="${preview}"
-			            data-source="${original}"
-			            alt="${description}"/>
-	            </a>
-            </div>`;
+    return `<a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}"
+            alt="${description}"/></a>`;
   })
   .join('');
 
 galleryContainer.insertAdjacentHTML('beforeend', imgMarkup);
 
-function openModal(currentImageUrl, currentImageAlt) {
-  const modalImg = basicLightbox.create(
-    `
-     <div class="modal">
-       <img
-      class="gallery__image--large"
-      src = "${currentImageUrl}"
-      alt = "${currentImageAlt}"
-    />
-    </div>
-`,
-    closeModalImg,
-  );
-
-  modalImg.show();
-}
-const closeModalImg = {
-  onShow: modalImg => {
-    modalImg.element().querySelector('img').onclick = modalImg.close;
-    window.addEventListener('keydown', onEscKeyPress);
-    function onEscKeyPress(event) {
-      if (event.code === 'Escape') {
-        window.removeEventListener('keydown', onEscKeyPress);
-        modalImg.close();
-      }
-    }
-  },
+const forOpenLightbox = {
+  captionsData: 'alt',
+  captionDelay: 250,
 };
-function onGalleryContainerClick(event) {
-  event.preventDefault();
 
-  const galleryCardEl = event.target.classList.contains('gallery__image');
-  if (!galleryCardEl) {
-    return;
-  }
-
-  const currentImageUrl = event.target.dataset.source;
-  const currentImageAlt = event.target.alt;
-
-  openModal(currentImageUrl, currentImageAlt);
-}
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+const lightbox = new SimpleLightbox('.gallery__link', forOpenLightbox);
