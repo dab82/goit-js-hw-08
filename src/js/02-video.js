edac40f049a862f8c2a400549904d05cad905806
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-10-29 00:04:33 +0300

    task 02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('play', onPlay => player.on('timeupdate', throttle(setCurrentTimeToStorage, 1000)));
+
+// function onPlay() {
+//   player.on('timeupdate', throttle(setCurrentTimeToStorage, 1000));
+// }
+
+function setCurrentTimeToStorage(event) {
+  localStorage.setItem(STORAGE_KEY, event.seconds);
+}
+
+getCurrentTimeFromStorage();
+
+function getCurrentTimeFromStorage() {
+  const storedTimeVideo = localStorage.getItem(STORAGE_KEY);
+  if (storedTimeVideo) {
+    player.setCurrentTime(storedTimeVideo);
+  }
+}
